2de9299555a8fa429ca9ddc404af9763153047c9
Aryaman Rishabh <aryamanrishabh@gmail.com>
2025-04-28 21:11:00 -0400

    add utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,13 @@ import { type ClassValue, clsx } from "clsx";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function getFileExtension(file: File) {
+  if (!file) return;
+
+  const mimeType = file.type;
+
+  const extension = mimeType.split("/")[1];
+
+  return extension;
+}
